9af2be572718d1c21f8a2b30f948a3da16604fd3
Salomao Rodrigues <salomao88@gmail.com>
2016-04-28 09:21:40 +0100

    Move components to a components directory

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import Board from './Board.jsx';
-import Peg from './Peg.jsx';
-import Row from './Row.jsx';
-import PegTray from './PegTray.jsx';
+import Board from './components/Board.jsx';
+import Peg from './components/Peg.jsx';
+import Row from './components/Row.jsx';
+import PegTray from './components/PegTray.jsx';
 
 class App extends React.Component {
 
