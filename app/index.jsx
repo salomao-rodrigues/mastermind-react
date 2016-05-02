fa1b2b489a4e8a6c6b764a1f4ef6794b7dbd07e7
Salomao Rodrigues <salomao88@gmail.com>
2016-05-02 11:41:20 +0100

    Add ability to have themes, create default theme

--- app/index.jsx
+++ app/index.jsx
@@ -12,7 +12,7 @@ class App extends React.Component {
 
   render () {
     return (
-      <div className="mastermind-game">
+      <div className="mastermind-game theme-default">
         <Board />
       </div>
     );
