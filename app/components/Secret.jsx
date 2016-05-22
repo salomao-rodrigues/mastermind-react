be81a18a0f1d1a29671a1f332ddf9a9fa2092fac
Salomao Rodrigues <salomao88@gmail.com>
2016-05-22 14:04:50 +0100

    Simplify more code

--- app/components/Secret.jsx
+++ app/components/Secret.jsx
@@ -3,24 +3,15 @@ import React from 'react';
 import Row from './Row.jsx';
 
 class Secret extends React.Component {
-  getSlots() {
-    if (this.props.revealed) {
-      return this.props.slots;
-    }
-  }
-
   render() {
+    const { revealed, slots, colors } = this.props;
     return (
       <Row
         className="mm-secret"
-        colors={this.props.colors}
-        slots={this.getSlots.call(this)} />
+        colors={colors}
+        slots={revealed && slots || undefined} />
     );
   }
 }
-
-Secret.defaultProps = {
-  revealed: false
-};
 
 export default Secret;
