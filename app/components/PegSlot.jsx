1811ccefeba0100c9af990fadef6093dd3b8a4b2
Salomao Rodrigues <salomao88@gmail.com>
2016-05-24 21:59:32 +0100

    Replace whole function with a boolean check

--- app/components/PegSlot.jsx
+++ app/components/PegSlot.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 
 class PegSlot extends React.Component {
-
-  renderActive() {
-    if (this.props.active === true) {
-      return <div className="active-slot" />;
-    }
-  }
-
   render() {
     return (
       <div className="mm-peg-slot">
-        {this.renderActive()}
+        {this.props.active && <div className="active-slot" />}
         <div className="mm-peg-slot-outer" />
         <div className="mm-peg-slot-inner" />
       </div>
