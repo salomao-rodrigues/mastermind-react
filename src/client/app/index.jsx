3b6f796dc8c97461bb0eb7475be3299b2009fadc
Salomao Rodrigues <salomao88@gmail.com>
2016-06-11 21:30:27 +0100

    Add react routes

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, Link, hashHistory } from 'react-router'
 
-import App from './containers/App.jsx';
+import App from './pages/App.jsx';
 import configureStore from './store/configureStore';
 
 import './stylesheets/main.scss';
 
 const store = configureStore();
 
+class ProviderWrapper extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
+  <Router history={hashHistory}>
+    <Route path="/" component={ProviderWrapper}>
+    </Route>
+  </Router>
+  , document.getElementById('app')
 );
